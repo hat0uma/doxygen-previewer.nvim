685102e1be1130364b07560ad10b86ac7a7779e4
rikuma-t <55551571+rikuma-t@users.noreply.github.com>
2023-06-29 23:51:40 +0900

    refactor:move viewer job to field

--- lua/doxygen-previewer/viewer.lua
+++ lua/doxygen-previewer/viewer.lua
@@ -1,23 +1,20 @@
 local util = require "doxygen-previewer.util"
 
---- @type number|nil
-local liveserver_job = nil
-
 --- @class DoxygenViewer
 --- @field open fun(opts:DoxygenPreviewerOptions,html_name:string)
 
---- @type table<string,DoxygenViewer>
-local M = {
-  ["live-server"] = {
-    open = function(opts, html_name)
-      local paths = util.previewer_paths(opts)
-      if liveserver_job ~= nil then
-        print "stop exists viewer job."
-        vim.fn.jobstop(liveserver_job)
-        liveserver_job = nil
-      end
-      liveserver_job = vim.fn.jobstart { opts.live_server, paths.temp_htmldir, "--open=" .. html_name }
-    end,
-  },
+--- @type DoxygenViewer
+local live_server = {}
+live_server._job = nil
+live_server.open = function(opts, html_name)
+  local paths = util.previewer_paths(opts)
+  if live_server._job ~= nil then
+    vim.fn.jobstop(live_server._job)
+    live_server._job = nil
+  end
+  live_server._job = vim.fn.jobstart { opts.live_server, paths.temp_htmldir, "--open=" .. html_name }
+end
+
+return {
+  ["live-server"] = live_server,
 }
-return M
